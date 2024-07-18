7af9214150babaf0671908dbbc8654b5f3d51e13
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2024-07-18 09:48:41 -0300

    Ordenação das linhas - implementação

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,3 +70,55 @@ const ajustarParaOperando1 = (operando) => {
     })
     
 }
+
+const ajustarParaOperando2 = (operando) => {
+    const algarismos = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    const linhasDeAlgarismosASeremExcluidas = algarismos.filter((algarismo) => !operando.includes(algarismo))//Algarismos que não pertencem ao operando 
+    linhasDeAlgarismosASeremExcluidas.forEach(algarismo => $(`tr[linha = "${algarismo}"]`).hide(2000))//Esconde as colunas indesejadas
+
+    
+    //Duplicar os devidos duplicados
+    let duplicados = []
+    operando.forEach(algarismo => {
+        let aparicoes = 0
+        operando.forEach(algarismo2 => {
+            if(algarismo == algarismo2) 
+                ++aparicoes
+        })
+
+        if(aparicoes > 1 && !duplicados.includes(algarismo)) {
+            let linha = $(`tr[linha = "${algarismo}"]`)       
+            $(`#tabela`).append(linha.clone())
+            duplicados.push(algarismo)
+        }
+    })
+
+
+    //Ordenar
+    /* 
+        Pega sempre a próxima coluna correspondente do algarismo e põe no final na ordem do operando
+    */
+   
+    operando.forEach(algarismo => {
+    
+            let linhas =  $(`tr[linha = "${algarismo}"]:not(.ordenado)`)
+
+            //linha que será a próxima a ser adiciona na sequência dos algarismos do operando
+            let linhaASerAdicionada = []
+            linhas.each(function( index ) { 
+                const buscaLinha = linhas.filter(linha => $(linha).attr("linha") == $(this).attr("linha"))
+                if(buscaLinha.length == 0) {
+                    linhaASerAdicionada.push($(this))
+                }
+            });
+               
+            let novaLinha = linhaASerAdicionada[0].clone()
+            novaLinha.addClass('ordenado')
+            $(`#tabela`).append(novaLinha)
+            linhaASerAdicionada[0].remove()
+            
+    })
+    
+    
+}
