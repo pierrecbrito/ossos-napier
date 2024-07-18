de5d18be11297fde5ab1db880f8cae16c51e9018
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2024-07-18 10:31:11 -0300

    Animação simpleste

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -8,3 +8,13 @@ const getCelulasVisiveis = () => {
 
     return resultado
 }
+
+const animar = () => {
+    let tempoInicial = 2000;
+    let iterador = 1
+    getCelulasVisiveis().reverse().forEach(celula => {
+        setTimeout(function() {
+            celula.addClass('destaque')
+        }, tempoInicial * iterador++)
+    })
+}
